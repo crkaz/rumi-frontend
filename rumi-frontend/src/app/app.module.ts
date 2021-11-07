0134621696ebceb6da77e94708cd46c6e096062e
CrkazDesktop <zakcatherall@outlook.com>
2021-11-07 18:11:37 +0000

    Updated folder structure

--- rumi-frontend/src/app/app.module.ts
+++ rumi-frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
